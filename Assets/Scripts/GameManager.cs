9784dfab2dad395d393b261375ec5c5fce135938
Kamal Sharma <36453230+kamalsh@users.noreply.github.com>
2019-03-25 20:14:52 +0530

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gamehasended = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void EndGame()
+    {
+        if (gamehasended == false)
+        {
+            gamehasended = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement Movement;
+
+	void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
